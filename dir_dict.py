02d702654ff8daf98bd3eacf5edc5a63fd25ae65
AnjaParsheva <windann168@gmail.com>
2018-05-04 14:02:11 +0300

    fix tempdir and path

--- dir_dict.py
+++ dir_dict.py
@@ -7,17 +7,13 @@ class DirDict:
 
     def __init__(self,path):
         self.path = path
-        self.cur_path = os.getcwd() +'/'+ self.path
-
-        try:
-            os.makedirs(path)
-        except OSError:
-            os.chdir('..')
+        self.cur_path = os.path.join(os.getcwd(),self.path)
+        os.makedirs(path)
 
     def __getitem__(self, key):
 
         try:
-            with open(self.cur_path + '/' + key, 'r') as f:
+            with open(os.path.join(self.cur_path,key), 'r') as f:
                 os.chdir('..')
                 return f.read()
         except FileNotFoundError:
@@ -31,7 +27,7 @@ class DirDict:
         os.chdir('..')
 
     def __delitem__(self, key):
-        os.remove(self.cur_path + '/'+ key)
+        os.remove(os.path.join(self.cur_path,key))
         os.chdir('..')
 
     def __len__(self):
@@ -53,7 +49,7 @@ class DirDict:
 
     def __iter__(self):
 
-        for elem in os.listdir(os.getcwd()+'/'+self.path):
+        for elem in os.listdir(os.path.join(os.getcwd(),self.path)):
             with open(elem,'r') as f:
                 yield f.read()
 
@@ -65,13 +61,13 @@ class DirDict:
             return False
 
     def __eq__(self, other):
-        if sorted(os.listdir(self.cur_path)) == sorted(os.listdir(os.getcwd() + '/' + other.path)):
+        if sorted(os.listdir(self.cur_path)) == sorted(os.listdir(os.path.join(os.getcwd(),other.path))):
             return True
         else:
             return False
 
     def __ne__(self, other):
-        if sorted(os.listdir(self.cur_path)) != sorted(os.listdir(os.getcwd() + '/' + other.path)):
+        if sorted(os.listdir(self.cur_path)) != sorted(os.listdir(os.path.join(os.getcwd(),other.path))):
             return True
         else:
             return False
@@ -86,7 +82,7 @@ class DirDict:
     def __str__(self):
         dictionary = {}
         for elem in os.listdir(self.cur_path):
-            with open(self.cur_path + '/' + elem, 'r') as f:
+            with open(os.path.join(self.cur_path,elem), 'r') as f:
                 dictionary[elem] = f.read()
         os.chdir('..')
         return str(dictionary)
@@ -99,25 +95,25 @@ class DirDict:
     def copy(self):
 
         os.mkdir(self.path + '_copy')
-        for elem in os.listdir(os.getcwd() + '/' + self.path):
-            with open(self.cur_path + '/' + elem, 'r') as f:
+        for elem in os.listdir(os.path.join(os.getcwd(),self.path)):
+            with open(os.path.join(self.cur_path,elem), 'r') as f:
                 data = f.read()
-            with open(self.cur_path + '_copy' + '/' + elem, 'w') as f:
+            with open(os.path.join(self.cur_path + '_copy',elem), 'w') as f:
                 f.write(data)
 
     def get(self, k, d=None):
-        if k in os.listdir(os.getcwd() + '/' + self.path):
-            with open(self.cur_path + '/' + k,'r') as f:
+        if k in os.listdir(os.path.join(os.getcwd(),self.path)):
+            with open(os.path.join(self.cur_path,k),'r') as f:
                 return f.read()
         else:
-            with open(self.cur_path + '/' + k,'w') as f:
+            with open(os.path.join(self.cur_path,k),'w') as f:
                 f.write(d)
                 return d
 
     def items(self):
         items = []
         for elem in os.listdir(self.cur_path):
-            with open(self.cur_path + '/' + elem, 'r') as f:
+            with open(os.path.join(self.cur_path,elem), 'r') as f:
                 items.append((elem,f.read()))
         os.chdir('..')
         return items
@@ -130,7 +126,7 @@ class DirDict:
 
     def pop(self, k, d=None):
         if k in os.listdir(self.cur_path):
-            with open(self.cur_path + '/' + k,'r') as f:
+            with open(os.path.join(self.cur_path,k),'r') as f:
                 del self[k]
                 os.chdir('..')
                 return f.read()
@@ -140,7 +136,7 @@ class DirDict:
 
     def popitem(self):
         k = choice(os.listdir(self.cur_path))
-        with open(self.cur_path + '/' + k,'r') as f:
+        with open(os.path.join(self.cur_path,k),'r') as f:
             del self[k]
             os.chdir('..')
             return k, f.read()
@@ -148,11 +144,11 @@ class DirDict:
     def setdefault(self, k, d=''):
         if k in os.listdir(self.cur_path):
 
-            with open(self.cur_path + '/' + k,'r') as f:
+            with open(os.path.join(self.cur_path,k),'r') as f:
                 return f.read()
 
         else:
-            with open(self.cur_path + '/' + k,'w') as f:
+            with open(os.path.join(self.cur_path,k),'w') as f:
                 f.write(str(d))
                 os.chdir('..')
                 return k, d
@@ -160,7 +156,7 @@ class DirDict:
     def update(self, *args):
         for elem in args:
             for k, v in elem.items():
-                with open(self.cur_path + '/' + k, 'w') as f:
+                with open(os.path.join(self.cur_path,k), 'w') as f:
                     f.write(v)
         os.chdir('..')
 
@@ -168,9 +164,9 @@ class DirDict:
         values = []
 
         for elem in os.listdir(self.cur_path):
-            with open(self.cur_path +'/' + elem, 'r') as f:
+            with open(os.path.join(self.cur_path,elem), 'r') as f:
                 values.append(f.read())
-        os.chdir('..')
+        #os.chdir('..')
         return values
 
 
--- tests/test_dir_dict.py
+++ tests/test_dir_dict.py
@@ -1,41 +1,56 @@
 import unittest
+import os
+import tempfile
 
 from dir_dict import DirDict
 
 class DirDictTestCase(unittest.TestCase):
+    tmpdirname = tempfile.TemporaryDirectory()
+
 
     def test_make_dict(self):
-        d = DirDict('dict1/storage')
+        tmpdirname = tempfile.TemporaryDirectory()
+        d = DirDict(os.path.join(str(tmpdirname),'dict/storage'))
         d['lang'] = 'Python'
         self.assertEqual('Python',d['lang'])
+        tmpdirname.cleanup()
 
     def test_del(self):
-        d = DirDict('dict2/storage')
+        tmpdirname = tempfile.TemporaryDirectory()
+        d = DirDict(os.path.join(str(tmpdirname), 'dict/storage'))
         d['lang'] = 'Python'
         del d['lang']
         self.assertEqual(0,len(d))
+        tmpdirname.cleanup()
 
     def test_keys(self):
-        d = DirDict('dict/storage')
+        tmpdirname = tempfile.TemporaryDirectory()
+        d = DirDict(os.path.join(str(tmpdirname), 'dict/storage'))
         d['lang'] = 'Python'
         d['lib'] = 'unittest'
         self.assertEqual(['lang','lib'], d.keys())
+        tmpdirname.cleanup()
 
     def test_values(self):
-        d = DirDict('dict/storage')
+        tmpdirname = tempfile.TemporaryDirectory()
+        d = DirDict(os.path.join(str(tmpdirname), 'dict/storage'))
         d['lang'] = 'Python'
         d['lib'] = 'unittest'
         self.assertEqual(['Python','unittest'], d.values())
+        tmpdirname.cleanup()
 
     def test_items(self):
-        d = DirDict('dict/storage')
+        tmpdirname = tempfile.TemporaryDirectory()
+        d = DirDict(os.path.join(str(tmpdirname), 'dict/storage'))
         d['lang'] = 'Python'
         d['lib'] = 'unittest'
         self.assertEqual([('lang','Python'),('lib','unittest')], d.items())
+        tmpdirname.cleanup()
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
 
